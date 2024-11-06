87940ae36a473ef15b307f8df12ed5d90db18715
mey <meydetour@gmail.com>
2024-11-06 22:54:38 +0100

    edit score_to_render

--- app/routes.py
+++ app/routes.py
@@ -35,11 +35,12 @@ def index():
     last_word = cemantix.get_last_said()
     words = cemantix.get_words()
     score_to_render = ""
-    for word in words :
-        if word[1] >= 0 :
-              score_to_render += f"<div class='word'><span class='{"lastWord" if word[0] == last_word else " "}'>{word[0]}</span><div class='range'><div class='progress' style='width : {word[1]}%; background :  linear-gradient(45deg, #3ff905, hsl({100-word[1]} 100% 43% / 1)'></div></div><span class='{"lastWord" if word[0] == last_word else " "}'>{word[1]}%</span></div>"
+    for word in words:
+        if word[1] >= 0:
+            score_to_render += f"<div class='word'><span class='{'lastWord' if word[0] == last_word else ''}'>{word[0]}</span><div class='range'><div class='progress' style='width: {word[1]}%; background: linear-gradient(45deg, #3ff905, hsl({100 - word[1]} 100% 43% / 1))'></div></div><span class='{'lastWord' if word[0] == last_word else ''}'>{word[1]}%</span></div>"
         else:
-            score_to_render += f"<div class='word'><span class='{"lastWord" if word[0] == last_word else " "}'>{word[0]}</span><div class='range'><div class='progress' style='width : {0}%; background :  linear-gradient(45deg, #3ff905, hsl({100-word[1]} 100% 43% / 1)'></div></div><span class='{"lastWord" if word[0] == last_word else " "}'>{word[1]}%</span></div>"
+            score_to_render += f"<div class='word'><span class='{'lastWord' if word[0] == last_word else ''}'>{word[0]}</span><div class='range'><div class='progress' style='width: 0%; background: linear-gradient(45deg, #3ff905, hsl({100 - word[1]} 100% 43% / 1))'></div></div><span class='{'lastWord' if word[0] == last_word else ''}'>{word[1]}%</span></div>"
+
     return render_template('index.html',
                            word = data,
                            done = True,
